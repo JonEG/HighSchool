45a3de732e4dd453c06662ab603dec3bf7188763
Jon Echeveste González <j.echeveste-gonzales@ovan.de>
2022-10-19 15:38:06 +0200

    Extended fe_users

---
+++ local_packages/class_year/Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,26 @@
+<?php
+defined('TYPO3') or die();
+
+// Add some fields to fe_users table to show TCA fields definitions
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users',
+   [
+        'tx_classyear_classroom' => [
+            'label' => 'LLL:EXT:classyear/Resources/Private/Language/locallang.xlf:classroom',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'foreign_table' => 'tx_classyear_domain_model_classroom',
+                'fieldWizard' => [
+                    'selectIcons' => [
+                        'disabled' => false,
+                    ],
+                ],
+            ],
+        ],
+   ]
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+   'fe_users',
+   'tx_classyear_classroom'
+);
--- local_packages/class_year/ext_tables.sql
+++ local_packages/class_year/ext_tables.sql
@@ -1,3 +1,6 @@
+/**
+* CUSTOM TABLES
+*/
 CREATE TABLE tx_classyear_domain_model_classroom (
   name varchar(255) DEFAULT '' NOT NULL,
 );
@@ -12,4 +15,12 @@ CREATE TABLE tx_classyear_domain_model_student (
 
 CREATE TABLE tx_classyear_domain_model_subject (
   title varchar(255) DEFAULT '' NOT NULL,
+);
+
+/**
+* TABLES OVERRIDES
+*/
+CREATE TABLE fe_users (
+  tx_classyear_classroom int(10) unsigned NOT NULL,
+  FOREIGN KEY (tx_classyear_classroom) REFERENCES tx_classyear_domain_model_classroom(uid)
 );
